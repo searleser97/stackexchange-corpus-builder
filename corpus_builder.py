342d2e562c7bb4bca8288bd67fd90e5d53868fab
searleser97 <gabrielsanv97@gmail.com>
2021-04-23 10:49:16 -0500

    First working version

--- corpus_builder.py
+++ corpus_builder.py
@@ -1,31 +1,79 @@
 import requests
 from os import path
-
-baseUrl = 'https://api.stackexchange.com/2.2/'
-
-mainUrl = 'questions?sort=desc'
-# url = '&pagesize=5&site=sports'
-pagesize = 100
+from typing import Any
 
 
-def getListOfSiteNames():
-    sitesUrl = path.join(baseUrl, 'sites?pagesize=500')
-    sitesReq = requests.get(sitesUrl)
-    sites = sitesReq.json()['items']
+class CorpusBuilder:
+    def baseUrl(self):
+        return 'https://api.stackexchange.com/2.2/'
 
-    siteNames = []
+    def getListOfSiteNames(self):
+        sitesUrl = path.join(self.baseUrl(), 'sites?pagesize=500')
+        sitesReq = requests.get(sitesUrl)
+        sites = sitesReq.json()['items']
 
-    for site in sites:
-        if site['site_type'] == 'main_site':
-            siteName = site['api_site_parameter']
-            siteNames.append(siteName)
+        siteNames = []
 
-    return siteNames
+        for site in sites:
+            if site['site_type'] == 'main_site':
+                siteName = site['api_site_parameter']
+                siteNames.append(siteName)
+                print(siteName)
+
+        return siteNames
+
+    def getQuestionsCount(self, siteName: str) -> int:
+        # url that returns the questions count
+        qCountUrl = path.join(self.baseUrl(),
+                              'questions') + f'?site={siteName}&filter=total'
+        qCount = requests.get(qCountUrl).json()['total']
+        return qCount
+
+    def getQuestions(self, siteName: str, count: int):
+        qUrl = path.join(self.baseUrl(),
+                         'questions') + f'?site={siteName}&pagesize=100'
+
+        allQuestions = []
+        pageIndex = 1
+
+        while (len(allQuestions) < count):
+            req = requests.get(qUrl + f'&page={pageIndex}').json()
+            questions = req['items']
+            for questionItem in questions:
+                if (len(allQuestions) >= count):
+                    break
+                allQuestions.append(questionItem)
+            pageIndex += 1
+
+        return allQuestions
+
+    def getLabeledQuestions(self, siteNameAsLabel=True, questionsPerSite=5000):
+        siteNames = self.getListOfSiteNames()
+        questionsWithLabels = []
+        for site in siteNames:
+            if (self.getQuestionsCount(site) >= questionsPerSite):
+                print(site, questionsPerSite)
+                for questionItem in self.getQuestions(site, questionsPerSite):
+                    # labels = questionItem['tags']
+                    labels = []
+                    if (siteNameAsLabel):
+                        labels.append(site)
+                    labeledQuestion = (labels, questionItem['title'])
+                    # questionsWithLabels.append(labeledQuestion)
+                    self.writeLabeledTextForFasttext(labeledQuestion)
+
+        return questionsWithLabels
+
+    def writeLabeledTextForFasttext(labeledQuestion: str):
+        with open('output.txt', 'utf8') as f:
+            labels = ''
+            for label in labeledQuestion[0]:
+                labels += f'__label__{label} '
+            f.write(f'{labels}{labeledQuestion[1]}\n')
 
 
-siteNames = getListOfSiteNames()
-siteNames.sort()
+cb = CorpusBuilder()
+labeledQs = cb.getLabeledQuestions(siteNameAsLabel=True, questionsPerSite=10)
 
-print(siteNames)
-
-print('count:', len(siteNames))
+for qItem in labeledQs:
+    print(qItem)
